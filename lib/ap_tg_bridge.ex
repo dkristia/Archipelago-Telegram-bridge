b1cd7a09e80be116108160cfbfda81117f25e13d
Daniel Kristiansson <daniel.i.kristiansson@gmail.com>
2024-12-14 12:38:54 +0200

    Generate Elixir project

---
+++ lib/ap_tg_bridge.ex
@@ -0,0 +1,18 @@
+defmodule ApTgBridge do
+  @moduledoc """
+  Documentation for `ApTgBridge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ApTgBridge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ap_tg_bridge_test.exs
@@ -0,0 +1,8 @@
+defmodule ApTgBridgeTest do
+  use ExUnit.Case
+  doctest ApTgBridge
+
+  test "greets the world" do
+    assert ApTgBridge.hello() == :world
+  end
+end
