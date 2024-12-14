b1054e8550b2c8d935e29bc3bdb23bfae3d5f029
Aaro Sonninen <aaro.sonninen@paivola.fi>
2024-12-14 16:05:58 +0200

    Optimise Telegram bot update handling

--- lib/telegram/telegram.ex
+++ lib/telegram/telegram.ex
@@ -1,17 +1,32 @@
 defmodule Telegram do
+  # Iterate over updates
+  def iterate([update | rest]) do
+    if rest != [] do
+      process_update(update)
+      iterate(rest)
+    else
+      process_update(update)
+    end
+  end
+
+  # Process a single update
+  def process_update(update) do
+    IO.puts(update.message.text)
+
+    Nadia.send_message(update.message.chat.id, update.message.text, [
+      {:reply_to_message_id, update.message.message_id}
+    ])
+
+    update.update_id + 1
+  end
+
   def proc_update(offset) do
     case Nadia.get_updates([{:offset, offset}]) do
-      {:ok, [update | _rest]} ->
-        IO.puts(update.message.text)
-
-        Nadia.send_message(update.message.chat.id, update.message.text, [
-          {:reply_to_message_id, update.message.message_id}
-        ])
-
-        update.update_id + 1
-
       {:ok, []} ->
         offset
+
+      {:ok, updates} ->
+        iterate(updates)
 
       {:error, error} ->
         IO.puts(:fuk)
