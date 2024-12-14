3a6ac3606f17aa95e1f0923390ada27d6e9ef437
Aaro Sonninen <aaro.sonninen@paivola.fi>
2024-12-14 15:25:36 +0200

    Add simple echoer

--- lib/ap_tg_bridge.ex
+++ lib/ap_tg_bridge.ex
@@ -16,7 +16,31 @@ defmodule ApTgBridge do
     :world
   end
 
+  def proc_update(offset) do
+    case Nadia.get_updates([{:offset, offset}]) do
+      {:ok, [update | _rest]} ->
+        IO.puts(update.message.text)
+
+        Nadia.send_message(update.message.chat.id, update.message.text, [
+          {:reply_to_message_id, update.message.message_id}
+        ])
+
+        update.update_id + 1
+
+      {:ok, []} ->
+        offset
+
+      {:error, error} ->
+        IO.puts(:fuk)
+        offset
+    end
+  end
+
+  def loop(offset) do
+    loop(proc_update(offset))
+  end
+
   def run do
-    IO.puts("Hello, world!")
+    loop(0)
   end
 end
